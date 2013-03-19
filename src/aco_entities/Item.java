daffe968c7e6387f4ad1609a27d46fed25594930
kyara-kat <kyara_kat@yahoo.com>
2013-03-19 20:03:29 +0200

    Update Item.java

--- src/aco_entities/Item.java
+++ src/aco_entities/Item.java
@@ -4,6 +4,12 @@ public class Item {
 	private boolean toMigrate;	
 	private int[] resourceDemand;
 	
+	public static final int MIPS_MAX = 7000;
+	public static final int CORES_MAX = 10;
+	public static final int RAM_MAX = 15;
+	public static final int STORAGE_MAX = 500;
+	public static final int BANDWIDTH_MAX = 4;
+	
 	public Item() {
 		resourceDemand = new int[Resource.values().length];
 		setToMigrate(false);
